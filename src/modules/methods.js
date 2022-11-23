bacc99b747930f06de7497c8d771bcbed68f1543
Augusto Seta <alseta97@gmail.com>
2022-11-23 14:32:28 -0300

    modules have been added to load the interface and perform the actions of the application.

---
+++ src/modules/methods.js
@@ -0,0 +1,2 @@
+export const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+export const getDay = (date) => new Date(date * 1000).getDay();
---
+++ src/modules/ui.js
@@ -0,0 +1,26 @@
+export const ui = () => {
+  const appWrapper = document.querySelector('.app-wrapper');
+  appWrapper.innerHTML = `
+    <div class='header'>
+      <h1>MY WEATHER APP</h1>
+      <form>
+        <label for='location'>
+          <span>LOCATION</span>
+          <input type='text' id='location' minlength=5 maxlength=15 required>
+        </label>
+        <button>search</button>
+      </form>
+    </div>
+
+    <div class='loading'>
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+
+    <div class='footer'>
+      <p>PROJECT MADE IT BY AUGUSTO SETA</p>
+    </div>
+
+  `;
+};
