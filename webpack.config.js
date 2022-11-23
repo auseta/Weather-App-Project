c0e4153a1322be3397fcf503e8b2305d962059ad
Augusto Seta <alseta97@gmail.com>
2022-11-23 14:31:04 -0300

    configure webpack.config.js file to manage font and image assets

--- webpack.config.js
+++ webpack.config.js
@@ -20,9 +20,17 @@ module.exports = {
         "use": { 
           "loader": "babel-loader", 
           "options": { 
-            "presets": [ "@babel/preset-env", ] 
+            "presets": [ "@babel/preset-env" ] 
           } 
         } 
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },
