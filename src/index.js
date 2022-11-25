aad2273d8fce903574332035c2c7484faf80c51d
Augusto Seta <alseta97@gmail.com>
2022-11-25 01:00:44 -0300

    minor changes added.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './styles/style.css';
 import { ui } from './modules/ui';
 import loadMainConent from './modules/loadContent';
+import { findLocation } from './modules/actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   ui();
   loadMainConent();
+  findLocation();
 });
